ed9c24e4f8c4a507ac68db6af79d47f0caa157f4
YuliiaDubyniuk <126833026+YuliiaDubyniuk@users.noreply.github.com>
2023-09-24 23:08:08 -0400

    almost done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Form } from './Form/Form';
+import { Contacts } from './Contacts/Contacts';
+import { SearchField } from './SearchField/SearchField';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    const contactId = nanoid();
+    // const { contacts } = this.state;
+    const newContact = {
+      id: contactId,
+      name,
+      number,
+    };
+
+    if (this.state.contacts.some(contact => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }))
+  };
+
+  changeFilter = (evt) => {
+    console.log(evt.target.value)
+    // this.setState({ filter: evt.target.value });
+  };
+   
+  deleteContact = (contactId) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.addContact}></Form>
+        <h2>Contacts</h2>
+        <SearchField value={filter} onChange={this.changeFilter}></SearchField>
+        <Contacts contacts={filteredContacts} deleteContact={this.deleteContact}></Contacts>
+      </div>
+    );
+  }
+}
